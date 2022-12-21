12a0d05d2c5b3cff398342935c89cb75f537f81d
George Clam <gclam77@gmail.com>
2022-12-20 17:26:47 -0700

    made navbar compo functional

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent {
+  isActive = false;
+
+  links = [
+    { linkName: 'Gallery', path: '.' },
+    { linkName: 'Artists', path: '.' },
+    { linkName: 'Events', path: '.' },
+    { linkName: 'Account', path: '.' },
+    { linkName: 'Musings', path: '.' },
+  ];
+
+  constructor(private readonly router: Router) {}
+
+  toggleMenu() {
+    this.isActive = !this.isActive;
+  }
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
+    this.isActive = false;
+  }
+}
